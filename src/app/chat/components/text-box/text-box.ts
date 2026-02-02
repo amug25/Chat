e7f0f5678ff34ddd0096f0dea7895b79e26ae2ba
amug25 <aidamunozgomez97@gmail.com>
2026-02-02 13:57:52 +0100

    Añadido toast

--- src/app/chat/components/text-box/text-box.ts
+++ src/app/chat/components/text-box/text-box.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IonToolbar } from '@ionic/angular/standalone';
+import { IonToolbar, IonToast } from '@ionic/angular/standalone';
 import { MessagesService } from 'src/app/chat/services/messages.service';
 
 @Component({
   selector: 'text-box',
-  imports: [IonToolbar, ReactiveFormsModule],
+  imports: [IonToolbar, ReactiveFormsModule, IonToast],
   templateUrl: './text-box.html',
 })
 export class TextBox {
@@ -16,15 +16,40 @@ export class TextBox {
     validators: [Validators.required, Validators.maxLength(500)],
   });
 
+  showToast = false;
+  toastMessage = '';
+
+  private openToast(msg: string){
+    this.toastMessage = msg;
+    this.showToast = true;
+  }
+
   onSend() {
+    if (this.messageControl.value.trim() === ''){
+      this.openToast('No puedes mandar un mensaje vacío.');
+      this.messageControl.reset('');
+      return;
+    }
+
     if (this.messageControl.invalid) {
       console.log('El mensaje debe tener entre 1 y 500 caracteres');
+      const error = this.messageControl.errors;
 
-      //TODO: Lanzar un mensaje visible de error.
+      if(error?.['required']){
+        this.openToast('Escribe un mensaje antes de enviar.');
+      }else if(error?.['maxlength']){
+        this.openToast('Máximo 500 caracteres.');
+      }else {
+        this.openToast('Mensaje no válido.');
+      }
+      return;
     }
 
     const text = this.messageControl.value;
-    if (!text) return;
+    if (!text){
+      this.openToast('Escribe un mensaje antes de enviar.');
+      return;
+    }
 
     this.messageService.addMessage(text);
     this.messageControl.reset('');
